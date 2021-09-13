0ea112b77b4430fc295d34fb0bfb999696aa5d12
Filizzola <juliofilizzola@hotmail.com>
2021-09-13 11:14:05 -0300

    criando estrutura do app

--- typing/src/App.js
+++ typing/src/App.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      Julio filizzola
+    <div className="container">
+      <div className="valid-keys">
+        <span className="matched" ></span>
+        <span className="remainder" ></span>
+      </div>
+      <div className="typed-keys">
+        
+      </div>
+      <div className="completed-words">
+        <ol>
+          <li></li>
+        </ol>
+      </div>
+      
     </div>
   )
 }
