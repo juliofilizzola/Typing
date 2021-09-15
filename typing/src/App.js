77702f2a8a677f2e48a040c2b9cb5fe207cec9dc
Filizzola <juliofilizzola@hotmail.com>
2021-09-15 14:03:47 -0300

    incrementando handlekeydown

--- typing/src/App.js
+++ typing/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
 function App() {
+  const[typedKeys, setTypedKeys] = React.useState([]);
+  const handleKeyDown = (e) => {
+    e.preventDefault();
+    const { key } = e;
+    setTypedKeys((prevTypedKeys) => {
+      return [...prevTypedKeys, key];
+    });
+
+  }
+
   return (
-    <div className="container">
+    <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
       <div className="valid-keys">
         <span className="matched">emer</span>
         <span className="remainder">son</span>
       </div>
       <div className="typed-keys">
-        
+        { typedKeys? typedKeys.join(" ") : null}
       </div>
       <div className="completed-words">
         <ol>
